b1aca3edbc80f08b45d95f05ded29c9cf9ba1cf8
Terje Norderhaug <terje@in-progress.com>
2019-09-30 15:34:45 -0700

    Distribution

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,10 @@ exports.useBlockstack = useBlockstack;
 exports.setContext = setContext;
 exports.initBlockstack = initBlockstack;
 exports.Blockstack = Blockstack;
+exports.useFile = useFile;
+exports.useFilesList = useFilesList;
+exports.useFileUrl = useFileUrl;
+exports.useFetch = useFetch;
 exports.useStored = useStored;
 exports.usePersistent = usePersistent;
 exports.Persistent = Persistent;
@@ -26,6 +30,8 @@ var _lodash = require("lodash");
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
 
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
 
 function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
@@ -33,8 +39,6 @@ function _nonIterableRest() { throw new TypeError("Invalid attempt to destructur
 function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
 
 function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 var defaultValue = {
   userData: null,
@@ -131,27 +135,118 @@ function Blockstack(props) {
     value: context
   }, props.children);
 }
-/* Persistent Context */
+
+function useFile(path) {
+  var _useStateWithGaiaStor = useStateWithGaiaStorage(path, {
+    reader: _lodash.identity,
+    writer: _lodash.identity
+  }),
+      _useStateWithGaiaStor2 = _slicedToArray(_useStateWithGaiaStor, 2),
+      value = _useStateWithGaiaStor2[0],
+      setValue = _useStateWithGaiaStor2[1];
+
+  return [value, !(0, _lodash.isUndefined)(value) ? setValue : null];
+}
+/*
+=======================================================================
+EXPERIMENTAL FUNCTIONALITY
+APT TO CHANGE WITHOUT FURTHER NOTICE
+=======================================================================
+*/
+
+/* Low-level hooks for Gaia file system */
 
 
-function useStateWithLocalStorage(storageKey) {
-  var stored = localStorage.getItem(storageKey);
-  var content = typeof stored != 'undefined' ? JSON.parse(stored) : null;
-  console.log("PERSISTENT local:", stored, _typeof(stored));
+function useFilesList() {
+  /* First value is a list of files, defaults to empty list
+     Second value is null then number of files when list is complete.
+     FIX: Is number of files useful as output? What about errors? */
+  var _useBlockstack = useBlockstack(),
+      userSession = _useBlockstack.userSession,
+      userData = _useBlockstack.userData;
 
-  var _useState = (0, _react.useState)(content),
+  var isUserSignedIn = !!userData;
+
+  var _useState = (0, _react.useState)([]),
       _useState2 = _slicedToArray(_useState, 2),
       value = _useState2[0],
       setValue = _useState2[1];
 
-  _react["default"].useEffect(function () {
-    localStorage.setItem(storageKey, JSON.stringify(value || null));
-  }, [value]);
+  var _useState3 = (0, _react.useState)(null),
+      _useState4 = _slicedToArray(_useState3, 2),
+      fileCount = _useState4[0],
+      setCount = _useState4[1];
 
-  return [value, setValue];
+  var appendFile = (0, _react.useCallback)(function (path) {
+    value.push(path);
+    return true;
+  });
+  (0, _react.useEffect)(function () {
+    if (userSession && isUserSignedIn) {
+      userSession.listFiles(appendFile).then(setCount)["catch"](function (err) {
+        return console.warn("Failed retrieving files list:", err);
+      });
+    }
+  }, [userSession, isUserSignedIn]);
+  return [value, fileCount];
 }
 
-function useStateWithGaiaStorage(path) {
+function useFileUrl(path) {
+  // FIX: Should combine with others?
+  var _useBlockstack2 = useBlockstack(),
+      userSession = _useBlockstack2.userSession,
+      userData = _useBlockstack2.userData;
+
+  var _useState5 = (0, _react.useState)(null),
+      _useState6 = _slicedToArray(_useState5, 2),
+      value = _useState6[0],
+      setValue = _useState6[1];
+
+  (0, _react.useEffect)(function () {
+    if (userSession) {
+      if (path) {
+        userSession.getFileUrl(path).then(setValue)["catch"](function (err) {
+          return console.warn("Failed getting file url:", err);
+        });
+      } else {
+        setValue(null);
+      }
+    }
+  }, [userSession, path]);
+  return value;
+}
+
+function useFetch(path, init) {
+  // For internal uses, likely better covered by other libraries
+  var url = useFileUrl(path);
+
+  var _useState7 = (0, _react.useState)(null),
+      _useState8 = _slicedToArray(_useState7, 2),
+      value = _useState8[0],
+      setValue = _useState8[1];
+
+  (0, _react.useEffect)(function () {
+    if (url) {
+      fetch(url, init).then(setValue)["catch"](function (err) {
+        return console.warn("Failed fetching url:", err);
+      });
+    } else {
+      setValue(null);
+    }
+  }, [url]);
+  return value;
+}
+
+function useStateWithGaiaStorage(path, _ref) {
+  var _ref$reader = _ref.reader,
+      reader = _ref$reader === void 0 ? _lodash.identity : _ref$reader,
+      _ref$writer = _ref.writer,
+      writer = _ref$writer === void 0 ? _lodash.identity : _ref$writer,
+      _ref$signed = _ref.signed,
+      signed = _ref$signed === void 0 ? false : _ref$signed,
+      _ref$initial = _ref.initial,
+      initial = _ref$initial === void 0 ? null : _ref$initial;
+
   /* Low level gaia file hook
      Note: Does not guard against multiple hooks for the same file
      Possbly an issue that change is set then value, could introduce inconsisitent state
@@ -161,31 +256,38 @@ function useStateWithGaiaStorage(path) {
        3. File not yet accessed or inaccessible -> undefined
      Attempting to set value when undefined throws an error.
   */
-  var _useState3 = (0, _react.useState)(undefined),
-      _useState4 = _slicedToArray(_useState3, 2),
-      value = _useState4[0],
-      setValue = _useState4[1];
+  var _useState9 = (0, _react.useState)(undefined),
+      _useState10 = _slicedToArray(_useState9, 2),
+      value = _useState10[0],
+      setValue = _useState10[1];
 
-  var _useState5 = (0, _react.useState)(undefined),
-      _useState6 = _slicedToArray(_useState5, 2),
-      change = _useState6[0],
-      setChange = _useState6[1];
+  var _useState11 = (0, _react.useState)(undefined),
+      _useState12 = _slicedToArray(_useState11, 2),
+      change = _useState12[0],
+      setChange = _useState12[1];
 
   var updateValue = function updateValue(update) {
-    console.log("[File] update:", path, update);
+    // ##FIX: properly handle update being a fn, call with (change || value)
+    console.log("[File] Update:", path, update);
 
     if (!(0, _lodash.isUndefined)(value)) {
-      setChange(update);
+      if ((0, _lodash.isFunction)(update)) {
+        setchange(function (change) {
+          return update(!(0, _lodash.isUndefined)(change) ? change : value);
+        });
+      } else {
+        setChange(update);
+      }
     } else {
-      throw "Premature attempt to modify file:" + path;
+      throw "Premature attempt to update file:" + path;
     }
   };
 
   console.log("[File]:", path, " = ", value);
 
-  var _useBlockstack = useBlockstack(),
-      userSession = _useBlockstack.userSession,
-      userData = _useBlockstack.userData;
+  var _useBlockstack3 = useBlockstack(),
+      userSession = _useBlockstack3.userSession,
+      userData = _useBlockstack3.userData;
 
   var isUserSignedIn = !!userData; // React roadmap is to support data loading with Suspense hook
 
@@ -194,7 +296,7 @@ function useStateWithGaiaStorage(path) {
       if (isUserSignedIn && path) {
         userSession.getFile(path).then(function (stored) {
           console.info("[File] Get:", path, value, stored);
-          var content = !(0, _lodash.isNil)(stored) ? JSON.parse(stored) : {};
+          var content = !(0, _lodash.isNil)(stored) ? reader(stored) : initial;
           setValue(content);
         })["catch"](function (err) {
           console.error("[File] Get error:", err);
@@ -209,22 +311,30 @@ function useStateWithGaiaStorage(path) {
     }
   }, [userSession, isUserSignedIn, path]);
   (0, _react.useEffect)(function () {
-    if (!(0, _lodash.isNil)(change)) {
+    if (!(0, _lodash.isUndefined)(change)) {
       if (!isUserSignedIn) {
         console.warn("[File] User not logged in");
       } else if (!(0, _lodash.isEqual)(change, value)) {
         // test should be redundant
-        var content = JSON.stringify(change);
-        var original = value;
-        setValue(change); // Cannot delay until saved as it will cause inconsistent state
+        if ((0, _lodash.isNull)(change)) {
+          userSession.deleteFile(path, {
+            wasSigned: signed
+          }).then(setValue(null))["catch"](function (err) {
+            return console.warn("Failed deleting:", path, err);
+          });
+        } else {
+          var content = writer(change);
+          var original = value; // setValue(change) // Cannot delay until saved? as it may cause inconsistent state
 
-        userSession.putFile(path, content).then(function () {
-          return console.info("[File] Put", path, content);
-        })["catch"](function (err) {
-          // Don't revert on error for now as it impairs UX
-          // setValue(original)
-          console.warn("[File] Put error: ", path, err);
-        });
+          userSession.putFile(path, content).then(function () {
+            console.info("[File] Put", path, content);
+            setValue(change);
+          })["catch"](function (err) {
+            // Don't revert on error for now as it impairs UX
+            // setValue(original)
+            console.warn("[File] Put error: ", path, err);
+          });
+        }
       } else {
         console.log("[File] Put noop:", path);
       }
@@ -232,6 +342,23 @@ function useStateWithGaiaStorage(path) {
   }, [change, userSession]); // FIX: deliver eventual error as third value?
 
   return [value, updateValue];
+}
+
+function useStateWithLocalStorage(storageKey) {
+  var stored = localStorage.getItem(storageKey);
+  var content = typeof stored != 'undefined' ? JSON.parse(stored) : null;
+  console.log("PERSISTENT local:", stored, _typeof(stored));
+
+  var _useState13 = (0, _react.useState)(content),
+      _useState14 = _slicedToArray(_useState13, 2),
+      value = _useState14[0],
+      setValue = _useState14[1];
+
+  _react["default"].useEffect(function () {
+    localStorage.setItem(storageKey, JSON.stringify(value || null));
+  }, [value]);
+
+  return [value, setValue];
 }
 
 function useStored(props) {
@@ -243,10 +370,14 @@ function useStored(props) {
   var version = props.version || 0;
   var path = props.path || property;
 
-  var _ref = props.local ? useStateWithLocalStorage(path) : useStateWithGaiaStorage(path),
-      _ref2 = _slicedToArray(_ref, 2),
-      stored = _ref2[0],
-      setStored = _ref2[1];
+  var _ref2 = props.local ? useStateWithLocalStorage(path) : useStateWithGaiaStorage(path, {
+    reader: JSON.parse,
+    writer: JSON.stringify,
+    initial: {}
+  }),
+      _ref3 = _slicedToArray(_ref2, 2),
+      stored = _ref3[0],
+      setStored = _ref3[1];
 
   (0, _react.useEffect)(function () {
     // Load data from file
@@ -363,10 +494,10 @@ function createAppManifestHook(appUri) {
 
 function useAppManifest(appUri) {
   // null when pending
-  var _useState7 = (0, _react.useState)(null),
-      _useState8 = _slicedToArray(_useState7, 2),
-      value = _useState8[0],
-      setValue = _useState8[1]; // ## FIX bug: May start another request while pending for a response
+  var _useState15 = (0, _react.useState)(null),
+      _useState16 = _slicedToArray(_useState15, 2),
+      value = _useState16[0],
+      setValue = _useState16[1]; // ## FIX bug: May start another request while pending for a response
 
 
   (0, _react.useEffect)(function () {
@@ -405,8 +536,8 @@ function AuthenticatedDocumentClass(props) {
   // declare a classname decorating the document element when authenticated
   var className = props.name;
 
-  var _useBlockstack2 = useBlockstack(),
-      userData = _useBlockstack2.userData;
+  var _useBlockstack4 = useBlockstack(),
+      userData = _useBlockstack4.userData;
 
   (0, _react.useEffect)(function () {
     console.log("Updating documentElement classes to reflect signed in status:", !!userData);
@@ -428,13 +559,13 @@ exports["default"] = _default;
 
 function useProfile(username, zoneFileLookupURL) {
   // FIX: don't lookup if username is current profile...
-  var _useState9 = (0, _react.useState)(null),
-      _useState10 = _slicedToArray(_useState9, 2),
-      value = _useState10[0],
-      setValue = _useState10[1];
+  var _useState17 = (0, _react.useState)(null),
+      _useState18 = _slicedToArray(_useState17, 2),
+      value = _useState18[0],
+      setValue = _useState18[1];
 
-  var _useBlockstack3 = useBlockstack(),
-      userSession = _useBlockstack3.userSession;
+  var _useBlockstack5 = useBlockstack(),
+      userSession = _useBlockstack5.userSession;
 
   (0, _react.useEffect)(function () {
     if (userSession && username) {
