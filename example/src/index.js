eebfb11ac9690f9eda72b9b2888698b266366588
Terje Norderhaug <terje@in-progress.com>
2019-10-12 15:17:03 -0700

    Update example using default export

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.js'
-import { initBlockstack } from 'react-blockstack'
+import blockstack from 'react-blockstack'
 import { Blockstack } from 'react-blockstack/dist/context'
 import { AppConfig } from 'blockstack';
 
@@ -11,6 +11,6 @@ import 'bootstrap/dist/css/bootstrap.css';
 import'./styles/style.css';
 
 const appConfig = new AppConfig()
-initBlockstack(appConfig)
+const { userSession  } = blockstack(appConfig)
 
 ReactDOM.render(<Router><Blockstack><App/></Blockstack></Router>, document.getElementById('root'));
